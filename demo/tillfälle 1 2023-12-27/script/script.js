c6bfc09613d8ee4ca6d40dda7975f5c9e3b1ed1c
Alrik He <alrik_he@hotmail.com>
2023-12-27 18:47:07 +0100

    T1 2023-12-27

---
+++ demo/tillfälle 1 2023-12-27/script/script.js
@@ -0,0 +1,25 @@
+/*
+alert(23456)
+document.write("hello")
+console.log(888 ) 
+*/
+
+
+// 1) variabler
+
+let foo // skapa en variabeln som heter foo
+
+foo = 5 // lägger in nummer 5 i foo 
+
+foo = 10
+
+foo = 20
+
+//foo = "hello"
+
+
+let bar = 30 //deklarera variabeln bar och tilldela med nummer 5
+
+console.log(foo)
+console.log(bar)
+console.log(1+1*2)
