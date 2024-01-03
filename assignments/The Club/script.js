45ff7b79aa27023cdf8cbf1b5a8f323d1d780429
MelissaT96 <155179170+MelissaT96@users.noreply.github.com>
2024-01-03 15:47:47 +0100

    Assignment 1.5 + Melissa
    Assignment 1.5 + Melissa

---
+++ assignments/The Club/script.js
@@ -0,0 +1,15 @@
+let vip = true
+
+let age = 17
+
+if (age >= 18) alert ("access granted")
+else (age < 18) alert ("access denied")
+
+let 18 = true
+
+if vip alert ("welcome in my best customer :)")
+
+
+
+
+
