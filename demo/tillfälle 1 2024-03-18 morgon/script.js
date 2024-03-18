eb892d9425c22e0a1c0719c2db8c765e0040aa72
Alrik He <alrik_he@hotmail.com>
2024-03-18 14:07:08 +0100

    tillfälle 1 morgon

---
+++ demo/tillfälle 1 2024-03-18 morgon/script.js
@@ -0,0 +1,73 @@
+/* 
+alert(123) 
+document.write(456) 
+*/
+// (1) VARIABLER
+// console.log(789)
+
+// console.clear() rensar konsolen
+
+let foo  // deklaration/skapar en variable som heter foo
+
+// undefined är tomt
+// value är också text samt siffor/ andra datatyper
+foo = 888 // tilldelning/ sätter in värden
+
+foo = "hello world" // skriver över
+
+foo = foo 
+
+console.log(foo) 
+
+let bar = 111 // deklaration + tilldelning
+
+bar = foo
+
+let alrikhe2 
+
+bar = 1 + 1         // kalkylation
+
+bar = (2 + 2) * 3 
+
+bar = "hej" + "san"  // konkatination
+
+bar = "hej" + 1     // "hej1"
+
+bar =  "hej" + 1 + 1  // "hej11"
+
+bar =  "hej" + (1 + 1)  // "hej2"
+
+bar =  1 + 1 +"hej"     // "2hej"
+
+
+// (2) DATATYPER
+
+let x
+        //number
+x = -5   // integer , long / heltal
+x = 1.5  // float , double / decimaltal
+
+        //String
+x = "hej"   //string / text
+x = '@'     //character, char / karaktär,tecken 
+
+        //boolean
+x = true   //boolean, bool / boolean
+
+        // undefined
+x = undefined   // undefined / odefinerat (tomt)
+
+console.log(typeof x);
+
+
+// STRING + STRING = STRING
+// STRING + NUMBER = STRING
+// STRING + BOOLEAN = STRING
+
+// NUMBER + NUMBER = NUMBER
+// NUMBER + BOOLEAN = NUMBER
+
+// BOOLEAN + BOOLEAN = NUMBER
+
+
+
