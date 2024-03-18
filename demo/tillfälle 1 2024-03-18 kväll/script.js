de46b73e1a979eeeb6b71897ccd802f50f54ba10
Alrik He <alrik_he@hotmail.com>
2024-03-18 22:17:30 +0100

    t1 kväll

---
+++ demo/tillfälle 1 2024-03-18 kväll/script.js
@@ -0,0 +1,32 @@
+
+// 1) Variabler
+
+let x   // deklarera en variabel x/skapa en variabel som heter x
+
+x = 888  // Tilldelning/lägger vi in 888 i x
+
+x = 999 // lägger vi in 999 i x
+
+let y = 555 // deklaration + tilldelning
+
+
+console.log( (5 + 5) * 3 )
+console.log( x + 1 )
+
+//Parentess
+//Exponent
+//Multi
+//Divi
+//Addi
+//Subtrak
+
+x = "Alrik " + "hello world"   // konkatinera
+
+x = "hej" + ( 1 + 1 ) 
+
+console.log( x )
+alert(x)
+
+
+
+
