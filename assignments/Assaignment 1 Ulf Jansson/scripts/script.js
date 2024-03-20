aa728a9c2cad024ed84c8b9b06e3056c98a0dbdb
GruntingMarrow <162809767+GruntingMarrow@users.noreply.github.com>
2024-03-20 18:27:15 +0100

    Ulf jansson uppgift 1

---
+++ assignments/Assaignment 1 Ulf Jansson/scripts/script.js
@@ -0,0 +1,23 @@
+
+
+
+   
+    var foo = 0;
+    var bar = 0;
+   
+
+    var result = foo / bar;
+    
+    
+    
+    if (!isNaN(result)) {
+        console.log("Resultatet är: " + result);
+    } else {
+        console.log("You can't divide us!");
+    }
+    
+    
+
+
+
+
