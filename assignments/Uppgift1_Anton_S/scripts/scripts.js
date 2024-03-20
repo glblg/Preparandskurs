2a9e82228a9d1787a594c9b29814a8c98be5743b
Astansgard <163855231+Astansgard@users.noreply.github.com>
2024-03-20 16:16:59 +0100

    Anton Stansgård Uppgift1

---
+++ assignments/Uppgift1_Anton_S/scripts/scripts.js
@@ -0,0 +1,16 @@
+var foo = 0;
+var bar = 0;
+
+/*console.log(foo/bar)
+document.write(foo/bar) */
+
+function log() {
+  if ( bar == 0 )
+  {
+    alert ('You cant divide us')
+  }
+  else 
+   {
+    console.log(foo/bar)
+   }
+}
