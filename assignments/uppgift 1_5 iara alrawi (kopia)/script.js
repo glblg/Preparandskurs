d0b883a237d99eaab2545ce6e9aca0d1e2ca585d
Iara-alrawi <164043505+Iara-alrawi@users.noreply.github.com>
2024-03-21 19:21:40 +0100

    Add files via upload

---
+++ assignments/uppgift 1_5 iara alrawi (kopia)/script.js
@@ -0,0 +1,12 @@
+let vip = true;
+let age = 16;
+
+if (vip == true) {
+    alert ("welcome in my best VIP customer :)")
+}
+else if (age >= 18) {
+    alert ("Access denied")
+}
+else {
+    alert (" Access denied")
+}
