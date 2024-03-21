fc1ad71f46b6c2cd8f957014ddc5a003907aa389
GruntingMarrow <162809767+GruntingMarrow@users.noreply.github.com>
2024-03-21 20:42:46 +0100

    Add files via upload

---
+++ assignments/Assignment1_5 Ulf Jansson/script.js
@@ -0,0 +1,27 @@
+document.getElementById("enterBtn").addEventListener("click", function () {});
+
+var age = parseInt(document.getElementById("age1").value);
+console.log(age);
+var vip = document.getElementById("vip").checked;
+console.log(vip);
+
+document.getElementById("enterBtn").addEventListener("click", function () {});
+
+if (vip) {
+  alert("Welcome in my best VIP costomer");
+  console.log("V.I.P Access");
+} else {
+  if (age >= 18) {
+    let granted = "Access granted";
+    alert(granted);
+
+    console.log("Välkommen, betala 400:- :)");
+  } else {
+    let denied = "acces denied";
+    alert(denied);
+    console.log("access denied, Du bör gå hem till mamma");
+  }
+}
+
+
+// jag får det inte riktigt att lira så hoppas på konstruktiv feedback
