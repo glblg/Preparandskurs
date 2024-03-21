749194dae060cba6b8d2553ab461fab4c148612e
optimissm <163523992+optimissm@users.noreply.github.com>
2024-03-21 22:56:19 +0100

    Assignement 1_5 Matilda Witting

---
+++ assignments/assignement.1_5/moriskan.js
@@ -0,0 +1,13 @@
+let vip = true
+let age = 17
+
+if (vip == true){
+    console.log("Welcome in, my best VIP costumer :)");
+}else if (age >= 18) {
+    console.log("Access granted!");
+    } else {
+    console.log("Access denied");
+}
+if (age >= 18){
+    console.log(true);
+}
