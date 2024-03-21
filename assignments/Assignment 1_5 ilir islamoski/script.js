e695f66164379ed9c6321a2f1bd0976e0be87685
Ilo2024 <163781478+Ilo2024@users.noreply.github.com>
2024-03-21 13:47:21 +0100

    Add files via upload

---
+++ assignments/Assignment 1_5 ilir islamoski/script.js
@@ -0,0 +1,14 @@
+let vip = true;
+
+let age = 16;
+
+if (vip == true) {
+    console.log("Welcome in my best VIP Customer :)");
+    alert("Access granted")
+}
+else if (age >= 17) {
+    alert("Access granted");
+  } 
+else {
+    alert("Access denied")
+} 
