f05a0746a73aa490763b0405a264f54df8ea2ebf
EmilyPignatelli <164228145+EmilyPignatelli@users.noreply.github.com>
2024-03-22 12:41:38 +0100

    Assignment 1_5 Emily Pignatelli

---
+++ assignments/Assignment1_5_EmilyPignatelli/club.js
@@ -0,0 +1,29 @@
+
+let age = parseInt(prompt("Enter your age:"));
+
+// under 18 år
+if (age < 18) {
+    // VIP tillgång
+    let hasVipAccess = confirm("Do you have VIP access?");
+
+    // Har VIP tillgång
+    if (hasVipAccess) {
+        //  VIP kod
+        let vipCode = prompt("Enter your VIP code:");
+
+        //  VIP koden för att komma in är 12345
+        if (vipCode === "12345") {
+            // om koden är godkänd, welcome in my best vip customer
+            alert("Welcome in my best vip customer");
+        } else {
+            // Ej godkänd kod, neka tillgång
+            alert("Invalid VIP code. Access denied.");
+        }
+    } else {
+        //har kunden ingen VIP, neka åtkomst
+        alert("Access denied.");
+    }
+} else {
+    // If the user is 18 years old or older, welcome them to the site
+    alert("Welcome in my best VIP customer!");
+}
