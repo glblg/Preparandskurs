451c176d37785d5be92ff5abec644877f91a41c7
Alrik He <alrik_he@hotmail.com>
2024-03-27 12:18:28 +0100

    feedback

--- assignments/Uppgift3_Anton_S/AntonHelpFunctions.js
+++ assignments/Uppgift3_Anton_S/AntonHelpFunctions.js
@@ -1,4 +1,15 @@
+
+
 function charRemove(myChar,myText){
+    console.log(myText.replaceAll("myChar",""));
+    
 
+   /*  let filtreradText=""
+    for(  let i = 0 ; i < myText.length ; i ++){
+        if(myText[i] != myChar)
+            filtreradText+=myText[i]
+        console.log(myText[i]);
 
-}
+    }
+    console.log(filtreradText);
+ */}
--- assignments/Uppgift3_Anton_S/CatObject.js
+++ assignments/Uppgift3_Anton_S/CatObject.js
@@ -1,12 +1,12 @@
 const cat ={alive:true,namn:"Gustav"};
 function myFunction(){
- cat.alive = (Math.random()* 10 + 1 > 5);
+ //cat.alive = (Math.random()* 10 + 1 > 5);
 
-    if (cat.alive) {
+ cat.alive = Math.random() > 0.5;
+
+    if (cat.alive) 
       alert("The cats name is " + cat.namn +" and is alive");
-    }
     else 
-    {
       alert("The cats name is "+ cat.namn +" and is dead");
-    }
+    
 }
