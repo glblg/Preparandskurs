7e6ae31c6baf7e7b869fbcf82ab130692bdc4bf3
Alrik He <alrik_he@hotmail.com>
2024-04-08 12:35:40 +0200

    Add files via upload

---
+++ demo/tillfälle 1 2024-04-08/script.js
@@ -0,0 +1,28 @@
+/*
+alert(123)
+document.write(456)
+console.log(789)
+*/
+
+// 1 Variablar
+
+// skriv: let namn
+let foo  // skapar en variabel 
+foo = 1234567 // lägger in 1234567 till foo
+console.log(foo)
+
+foo = 5  // tilldela värdet 888 till variabeln foo
+console.log(foo)
+
+let bar = 5 // deklarera variabeln bar med 12345
+console.log( 5 + 5 )
+// pemdas systemet 
+
+bar = "hej" // text , string
+console.log( bar + foo + foo )
+// 
+console.log(  5 + 5 +"hej" )
+console.log(  "hej" + (5 + 5)  )
+
+
+//console.log(foo)
